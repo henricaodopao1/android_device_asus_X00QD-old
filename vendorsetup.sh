e492e6fc92625d9546fc3873b3ba61a06763f599
henricaodopao <allricknation@gmail.com>
2021-06-01 04:51:40 +0000

    X00QD: Track EVA GCC

---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm64.git prebuilts/gcc/linux-x86/aarch64/aarch64-elf
+git clone --depth=1 https://github.com/mvaisakh/gcc-arm.git prebuilts/gcc/linux-x86/arm/arm-eabi
