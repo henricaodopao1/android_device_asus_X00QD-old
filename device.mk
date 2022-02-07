4749865f3610414c63e430a35b8f0f50bb5f42bb
xyzuan <xyzuannihboss@gmail.com>
2022-02-07 08:04:33 +0000

    X00QD: overlay-xd: Declare X00QD information and adapt card radius with our screen corner

--- device.mk
+++ device.mk
@@ -420,7 +420,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-xdroid
     
 # Configstore
 PRODUCT_PACKAGES += \
