ae8cc713167d95dc8c2176b1821a02d5f7482ccc
RONIN808 <shubhamshedage808@gmail.com>
2022-03-14 20:46:48 +0530

    X00QD: build android.hidl.allocator@1.0
    Signed-off-by: harshhaareddy <harshhaa03@gmail.com>

--- device.mk
+++ device.mk
@@ -286,6 +286,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
+    android.hidl.allocator@1.0 \
+    android.hidl.allocator@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
